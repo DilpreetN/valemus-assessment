fe42e4566ba158b56656a0d9219d8a0b53483eba
DilliSN <dilli@dillisn.com>
2026-06-02 22:37:10 +0200

    [Chore] Introduce flake nix for development

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Frontend dev environment";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.nodejs_24
+          pkgs.pnpm
+        ];
+
+        shellHook = ''
+          echo "🚀Dev Environment Loaded"
+          echo "- Node.js:    $(node -v 2>/dev/null || echo 'not installed')"
+          echo "- pnpm:        $(pnpm -v 2>/dev/null || echo 'not installed')"
+        '';
+      };
+    };
+}
